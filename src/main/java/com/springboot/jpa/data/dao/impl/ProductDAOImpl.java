1c2d7df8a17793d2df7b6195c4d4a45547e5065a
GimHayeong-eng <hychunjae2021@gmail.com>
2023-03-03 16:02:10 +0900

    DaoNDto

---
+++ src/main/java/com/springboot/jpa/data/dao/impl/ProductDAOImpl.java
@@ -0,0 +1,61 @@
+package com.springboot.jpa.data.dao.impl;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.springboot.jpa.data.dao.ProductDAO;
+import com.springboot.jpa.data.entity.Product;
+import com.springboot.jpa.data.entity.ProductRepository;
+
+public class ProductDAOImpl implements ProductDAO {
+    private final ProductRepository productRepository;
+
+    @Autowired
+    public ProductDAOImpl(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    @Override
+    public Product insertProduct(Product product) {
+        Product savedProduct = productRepository.save(product);
+        return savedProduct;
+    }
+
+    @Override
+    public Product selectProduct(Long number) {
+        Product selectedProduct = productRepository.getReferenceById(number);
+        return selectedProduct;
+    }
+
+    @Override
+    public Product updateProduct(Long number, String name) throws Exception {
+        Optional<Product> selectedProduct = productRepository.findById(number);
+
+        if (selectedProduct.isPresent()) {
+            throw new Exception();
+        }
+
+        Product updatedProduct;
+        Product product = selectedProduct.get();
+        product.setName(name);
+        product.setUpdateAt(LocalDateTime.now());
+
+        updatedProduct = productRepository.save(product);
+        return updatedProduct;
+    }
+
+    @Override
+    public void deleteProduct(Long number) throws Exception {
+        Optional<Product> selectedProduct = productRepository.findById(number);
+
+        if(selectedProduct.isPresent()) {
+            throw new Exception();
+        }
+
+        Product product = selectedProduct.get();
+        productRepository.delete(product);
+    }
+    
+}
